1042fca77663d381aedbd6aecc37e19cd0ccad61
maaz siddiqui <maaz.siddiqui3073@gmail.com>
2020-09-19 02:16:00 +0500

    commiting after 2nd major changes

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ function Get(yourUrl){
 }
 
 function submit(){
-    const basecurrency = document.getElementById('currencyfrom');
-    const pkrCurrency = document.getElementById('currency'); 
+    const basecurrency = document.getElementById('currencyfrom');//string currnecy name
+    const pkrCurrency = document.getElementById('currency'); //number
+    if(pkrCurrency.value.length == '0'|| pkrCurrency.value < 0){
+       pkrCurrency.value = 0;
+    }else if(pkrCurrency.value.length > '0'){
+        pkrCurrency.value = pkrCurrency.value-0;
+    }
+    
     const convert = document.getElementById('convert');
     responseObj = Get(`https://v6.exchangerate-api.com/v6/d108e4b90d10abf082636e69/latest/${basecurrency.value}`);  
     const parseObj = JSON.parse(responseObj);
@@ -17,37 +23,22 @@ function submit(){
     output.innerText = `Converted : ${(pkrCurrency.value * parseObj.conversion_rates[convert.value]).toFixed(2)}  ${convert.value}` ;
 
 }
-
+submit();
+function currencyReverse(){
+    var basecurrency = document.getElementById('currencyfrom').value;
+    var abovecurrency = document.getElementById('convert').value;
+    var tempCurrency =  basecurrency;
+    basecurrency = abovecurrency;
+    abovecurrency = tempCurrency;//working
+    document.getElementById('currencyfrom').value = basecurrency;
+    document.getElementById('convert').value = abovecurrency;
+    submit();
+}
 var input = document.getElementById("currency");
 
-input.addEventListener('keyup', function(event) {
-    if (event.keycode === 13) {
-      event.preventDefault();
-      submit();
-    }
-  });
 
 function darkmode(){
-    // const bodytag = document.getElementsByTagName('body');
-    // bodytag[0].classList.toggle('active'); git checl
     document.documentElement.classList.toggle('active');
 }
 
-// responseObj = Get(`https://v6.exchangerate-api.com/v6/d108e4b90d10abf082636e69/latest/${basecurrency.value}`);
-
-// console.log(parseObj.conversion_rates);
-
-
-// const data = document.createElement('p');
-// data.innerText = JSON.stringify(parseObj.conversion_rates);
-
-// let a = pkrCurrency.value;
-// let b = parseObj.conversion_rates[convert.value];
-// var converted = a*b ;
-
-
-
-
-// const body = document.querySelector('body');
-// body.appendChild(data);
 
